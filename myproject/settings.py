8f26213e9a9d718e2419421b7a46a8ac28a917f9
arunima <arunimatka@gmail.com>
2026-01-18 12:27:42 +0530

    Fix: Restore DATABASES variable definition in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -93,6 +93,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
